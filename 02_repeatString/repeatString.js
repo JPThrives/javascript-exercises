464df04db35238c2cc6b639434222594fdf00ebb
JPThrives <boruahjp1999@gmail.com>
2024-07-16 20:48:09 +0530

    changed error to ERROR

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,18 +1,18 @@
 const repeatString = function(word,number) {
         if (number < 0) {
-            return "Error";
+            return "ERROR";
         }
         let string = "";
         for (let i = 0;i < number; i++) {
-            return string += word;
+            string += word;
         }
+        return string;
 }
 repeatString('hey',3);
 repeatString('hello',10);
 repeatString('hi',1);
 repeatString('bye',0);
 repeatString('goodbye',-1);
-repeatString('odin',number);
 repeatString('',10);
 
 // Do not edit below this line
