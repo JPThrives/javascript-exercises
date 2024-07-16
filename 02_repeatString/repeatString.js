d14f73a43b963e16ecade78ea07332fb27642238
JPThrives <boruahjp1999@gmail.com>
2024-07-16 20:09:54 +0530

    solved 02 repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,19 @@
-const repeatString = function() {
-
-};
+const repeatString = function(word,number) {
+        if (number < 0) {
+            return "Error";
+        }
+        let string = "";
+        for (let i = 0;i < number; i++) {
+            return string += word;
+        }
+}
+repeatString('hey',3);
+repeatString('hello',10);
+repeatString('hi',1);
+repeatString('bye',0);
+repeatString('goodbye',-1);
+repeatString('odin',number);
+repeatString('',10);
 
 // Do not edit below this line
 module.exports = repeatString;
