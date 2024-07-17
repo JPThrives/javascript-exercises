3615863eb28226ec96efc6272a843cc0878dfdfc
JPThrives <boruahjp1999@gmail.com>
2024-07-17 12:37:14 +0530

    solved 07 tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempinFahrenheit) {
+    let celsius = (tempinFahrenheit - 32)*5/9;
+    return Math.round(celsius * 10)/10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempinCelsious) {
+    let fahrenheit = tempinCelsious*9/5 +32;
+    return Math.round(fahrenheit * 10)/10;
 };
 
 // Do not edit below this line
